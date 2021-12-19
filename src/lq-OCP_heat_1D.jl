a820be2b0d175f1c4bc7093753f0dd591888f668
jayborkhatariya17 <89299367+jayborkhatariya17@users.noreply.github.com>
2021-12-19 17:47:41 +0100

    Update lq-OCP_heat_1D.jl

--- src/lq-OCP_heat_1D.jl
+++ src/lq-OCP_heat_1D.jl
@@ -1,4 +1,4 @@
-using JuMP, Ipopt, LinearAlgebra
+using JuMP, Ipopt, Plots, LinearAlgebra, SparseArrays
 
 #A JuMP model is initialized
 mod = Model(optimizer_with_attributes( Ipopt.Optimizer ,  "max_iter" => 100,
@@ -16,62 +16,64 @@ N = 11                                 #Discretization points / nodes
 Δx = L / (N-1)                         # Δx = x[i+1] - x[i]
 
 #Integration from t0 = 0 to tf
-tf = 900.0                             #Final time: 900 seconds = 15 minutes
-Δt = 1.0                               #Sampling period
+tf = 10000.0;                          #Final time
+Δt = 10^(-1);                          #Sampling period
 k = round(Int,tf / Δt);                #k = number of steps
 
+#System's matrix: θ' = Aθ + Bu
+A = spdiagm(-1 => ones( N-1), 0 => -2*ones(N), 1 => ones(N-1));
+A[2,1] = 2.0;
+A[end - 1,end] = 2.0;
+A = (α/( Δx^2)) * A;
 
-#Initial and reference temperatures in Kelvin
+#input matrix
+B = spzeros(N);
+B[1] = 1;
+B = (2/ (c * ρ * Δx)) * B;
+
+#output matrix
+C = spzeros(1,N)
+C[1,end] = 1.0;
+
+#Initial and reference temperatures in kelvin
 θinit = 273.0
 θref = 500.0
-
 #bounded input values[u_min,u_max]
 u_min = 0.0
-u_max = 20e5; 
+u_max = 15000.0
 
 #Decision variables of the optimization problem
 @variable(mod,θ[1:N,1:k]);                              # temperature at x[1:N] at time step k
 @variable(mod,u_min <= u[1:k - 1] <= u_max);            # bounded constrained Input u
 @variable(mod, y[1:k]);                                 # Output y
-
 #Initial values
 @constraint(mod, θ[:,1] .== θinit)
 @constraint(mod, y[1] .== θinit)
-
-
 #System dynamics
-for j in 1:(k-1)
-
-    for i in 2:N-1
-        @constraint(mod,θ[i,j + 1] ==  θ[i,j] + Δt * α/( Δx^2)*(θ[i-1,j] - 2*θ[i,j] + θ[i+1,j]) ) # Diffusion at inner grid points
-    end
-    @constraint(mod, θ[1,j + 1] == (θ[1,j] + Δt*( α/(Δx^2) * (-2*θ[1,j] + 2*θ[2,j]) + 2/(c * ρ * Δx)*u[j]))) # Left side: heat input
-    @constraint(mod, θ[end,j + 1] == ( θ[end,j] + Δt * α/(Δx^2) * (2*θ[end-1,j] - 2*θ[end,j]) )) # Right side: isolated side
-    @constraint(mod,y[j + 1] ==  θ[end,j + 1]) # Measurement at left side
+for j in 1:k - 1,i in 1:N
+    @constraint(mod,θ[i,j + 1] - (θ[i,j] + Δt * ((A[:,i]' * θ[:,j]) + B[i] * u[j])).== 0.0)
 end
 
-q_w = 1000; # Weighing coefficient for errors (or states)
-r_w = 1e-4; # Weighing coefficient for input signals
+for j in 1:k - 1
+    @constraint(mod,y[j + 1] .== (C * θ[:,j + 1]))
+end
+#Weighing matrices
+Q = C' * C * 1000.0;
+R = 0.0001;
 
-# e(t) = r(t) - y(t) 
-# err = q * sum( e(t_i)^2 )  with index i ∈ (1,k-1)
-err = @NLexpression(mod, sum(q_w * (θref - y[j])^2 for j in 1:k-1) )
+#err = sum(e' Q e)
+err= @NLexpression(mod,(sum(Q[i,i] * (θref - θ[i,j]) ^ 2
+                        for j in 1:k-1,i in 1:N)))
+#in_err = sum(u'Ru)
+in_err = @NLexpression(mod,(sum(R * u[j] ^ 2 for j in 1:k-1)))
 
-#in_err = r * sum( u(t_i)^2)
-in_err = @NLexpression(mod, sum(r_w * u[j] ^ 2 for j in 1:k-1) )
-
-# Cost function
 J = @NLexpression(mod,0.5 * Δt * (err + in_err))
-
 #defining the cost function
 @NLobjective(mod, Min, J)
-
 #optimizes the model
 optimize!(mod)
 
-
-# Plotting the results
-using Plots
+#plotting values
 states_θstart = zeros(k)
 states_θend = zeros(k)
 input_u = zeros(k - 1)
@@ -81,11 +83,11 @@ for i = 1 : k-1
     input_u[i] = JuMP.value(u[i])
 end
 
-states_θstart[end] = JuMP.value(θ[1,end])
 states_θend[end] = JuMP.value(θ[end,end])
+states_θstart[end] = JuMP.value(θ[1,end])
 
-
-plot(states_θstart,label = "Left side")
-plot!(states_θend,label = "Right side")
-
-plot(input_u, label="Input")
+plot(states_θstart,label = "θ1")
+plot!(states_θend,label="y = Cθ")
+savefig("lq-optimal_1D_Q1000_R0001_output-bounded.png")
+p3 = plot(input_u,label="input,Q = 1000,R = 0.001")
+savefig("lq-optimal_1D_Q1000_R0001-input-bounded.png")
